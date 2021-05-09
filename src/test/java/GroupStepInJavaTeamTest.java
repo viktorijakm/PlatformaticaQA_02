2c13f3ebc780502ac65e3f2de661fb7532045e4a
Vitaly <vzhka@post.cz>
2021-05-10 01:33:35 +0300

    Добавлен тест Зверева Виталия на сайта https://ya.ru/

--- src/test/java/GroupStepInJavaTeamTest.java
+++ src/test/java/GroupStepInJavaTeamTest.java
@@ -1,9 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class GroupStepInJavaTeamTest {
 
@@ -17,6 +20,10 @@ public class GroupStepInJavaTeamTest {
     @BeforeMethod
     public void beforeTest() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
     }
 
     @Test
@@ -30,6 +37,21 @@ public class GroupStepInJavaTeamTest {
         driver.findElement(By.xpath("//div[@data-title=\"Web element\"]")).click();
         Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(), "Web element");
         Thread.sleep(5000);
+    }
+
+    @Test
+    public void vitalyZverevTest() {
+
+        String value = "Selenium";
+
+        driver.get("https://ya.ru/");
+
+        WebElement input = driver.findElement(By.xpath("//span[@class='input__box']/input"));
+        input.sendKeys(value + "\n");
+
+        WebElement result = driver.findElement(By.xpath("//div[contains(@class, 'serp-title_font')]"));
+
+        Assert.assertEquals(result.getText(), value);
     }
 
     @AfterMethod
