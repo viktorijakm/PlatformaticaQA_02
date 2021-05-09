3fc9ba2582e03724b19dcfab705258859c1d1131
PRoman-86 <pkomputer17@gmail.com>
2021-05-09 23:34:42 +0300

    commit01

---
+++ src/test/java/Groupgroup_alla.java
@@ -0,0 +1,49 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Groupgroup_alla {
+
+    public static boolean tenMinuteMailTest() {
+
+        String currentMail = "";
+        //System.setProperty("webdriver.chrome.driver", "C:/_install/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        WebDriver chromedrv = new ChromeDriver();
+
+
+        try {
+            chromedrv.get("https://10mail.org/");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            WebElement mailArea = chromedrv.findElement(By.className("address"));
+            currentMail = mailArea.getText();
+
+            if (currentMail.equals(""))
+                return false;
+
+            System.out.println("Received mail: " + currentMail);
+        } finally {
+
+            chromedrv.quit();
+
+        }
+
+        return true;
+    }
+
+    @Test
+    public void poptsovRomanTest() {
+        Assert.assertEquals(tenMinuteMailTest(), true, "test failed");
+    }
+
+}
