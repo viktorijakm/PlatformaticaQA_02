ed0505cb54ad9c620651a58ceee26e14fc4e7bd4
NYerlan <75294179+NYerlan@users.noreply.github.com>
2021-05-09 14:55:33 -0700

    first test for zoni.edu by NYerlan (#12)
    * first test for zoni.edu by NYerlan
    
    * changed class name
    
    * changed to method Click() in 27th line

---
+++ src/test/java/GroupSquirrelsTest.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class GroupSquirrelsTest {
+
+    @BeforeClass
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void nYerlan() {
+
+        driver.get("https://www.zoni.edu/");
+        driver.findElement(By.xpath("//*[@id=\"menu-item-11815\"]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"panel-10867-1-1-0\"]/div/div/a/img")).click();
+        WebElement txtStandardProgram = driver.findElement(By.xpath("//*[@id=\"panel-19455-0-0-0\"]/div/h3"));
+        Assert.assertEquals(txtStandardProgram.getText(),"Standard English Program");
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.quit();
+    }
+}
