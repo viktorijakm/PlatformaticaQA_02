a985d5873413ed4915d6b2b4dacf54e612b7474f
korikuman <korik_uman@yahoo.com>
2021-05-12 12:00:28 -0500

    method name changed to comply with requirements before test method updated comments removed Assertion added to test 1 variable name changed

--- src/test/java/KorikUmanTest.java
+++ src/test/java/KorikUmanTest.java
@@ -11,40 +11,41 @@ import org.testng.annotations.Test;
 
 public class KorikUmanTest {
     @BeforeClass
-    public void before() {WebDriverManager.chromedriver().setup();}
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+    }
 
     public WebDriver driver;
 
     @BeforeMethod
-    public void beforeTest() { driver = new ChromeDriver();}
+    public void beforeTest() {
+        driver = new ChromeDriver();
+        driver.get("https://ultimateqa.com/");
+        driver.manage().window().maximize();
+    }
 
     @Test
-    public void vKorenevskyyTest() throws InterruptedException {
-        //navigate to website and click on hyperlink
-        driver.get("https://ultimateqa.com/");
+    public void testOneVkorenevskyy() throws InterruptedException {
 
         WebElement input = driver.findElement(By.xpath("//*[@href='https://ultimateqa.com/selenium-webdriver-java-course/']"));
         input.click();
 
-        //assert expected element is displayed
-
-        Boolean result = driver.findElement(By.className("et_pb_video_overlay_hover")).isDisplayed();
+        boolean result = driver.findElement(By.className("et_pb_video_overlay_hover")).isDisplayed();
+        Assert.assertTrue(result);
     }
 
     @Test
-    public void vKorenevskyyTestTwo() throws InterruptedException {
-        //navigate to website
-        driver.get("https://ultimateqa.com/");
-        //locate search field
-        WebElement search = driver.findElement(By.xpath("//*[@name='s'][@class='et_pb_s']"));
-        //send search text
-        search.sendKeys("Selenium Java\n");
-        //locate target element
+    public void testTwoVkorenevskyy() throws InterruptedException {
+
+        WebElement searchField = driver.findElement(By.xpath("//*[@name='s'][@class='et_pb_s']"));
+
+        searchField.sendKeys("Selenium Java\n");
+
         WebElement result = driver.findElement(By.className("jetpack-instant-search__search-results-title"));
-        //declare veriables
+
         String expected = "Found 10 results";
         String actual = result.getText();
-        //validate test
+
         Assert.assertEquals(actual, expected);
     }
 
