63b54d198482021ac26fa330b8de515540f38a9b
Tor-YA <69291928+Tor-YA@users.noreply.github.com>
2021-05-12 14:41:31 +0300

    new test oz.by

---
+++ src/test/java/ViktoriaAnatskoTest.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ViktoriaAnatskoTest {
+
+    @BeforeClass
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://oz.by/");
+
+        WebElement input = driver.findElement(By.id("top-s"));
+        input.sendKeys("карандаш\n");
+
+        WebElement result = driver.findElement(
+                By.xpath("//*[@id=\"goods-table\"]/li[1]/div/div/div/div/a/p[1]"));
+        Assert.assertEquals(result.getText(), "Карандаш чернографитный с ластиком \"Party Blackline\" (HB)");
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.quit();
+    }
+}
