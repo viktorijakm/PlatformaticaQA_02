902bafb5b13a2035957874a3ee8d241fd79f5abf
Penny-Hofstadter <rnasibullina@gmail.com>
2021-05-13 12:08:07 -0400

    modified SergioReviewLanaTest.java

--- src/test/java/SergioReviewLanaTest.java
+++ src/test/java/SergioReviewLanaTest.java
@@ -1,5 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,6 +9,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class SergioReviewLanaTest {
 
@@ -35,6 +38,48 @@ public class SergioReviewLanaTest {
          driver.navigate().forward();
          driver.navigate().refresh();
         }
+        public static void newClick(WebDriver driver, WebElement element) {
+            JavascriptExecutor executor = (JavascriptExecutor) driver;
+            executor.executeScript("arguments[0].click()", element);
+
+
+        }
+
+        @Test
+        public void newTest2 () throws InterruptedException {
+            driver.get("https://demoqa.com/automation-practice-form");
+            driver.findElement(By.id("firstName")).sendKeys("John");
+            Thread.sleep(2000);
+            driver.findElement(By.id("lastName")).sendKeys("Smith");
+            Thread.sleep(2000);
+            driver.findElement(By.id("lastName")).clear();
+            Thread.sleep(2000);
+            driver.findElement(By.id("lastName")).sendKeys("Brown");
+            Thread.sleep(2000);
+            driver.findElement(By.id("lastName")).clear();
+
+
+            WebElement textName = driver.findElement(By.id("lastName"));
+            textName.sendKeys("Smith");
+            Thread.sleep(3000);
+//
+//            driver.findElement(By.xpath("//input[@name='gender'][@value='Male']")).click();
+//            Thread.sleep(2000);
+            List<WebElement> gender = driver.findElements(By.xpath("//input[@type = 'radio']"));
+            gender.size();
+            for (int i = 0; i < gender.size(); i++) {
+                String value = gender.get(i).getAttribute("value");
+                Thread.sleep(2000);
+                if (value.equalsIgnoreCase("male")) {
+                    newClick(driver, gender.get(i));
+                    break;
+                }
+                Thread.sleep(2000);
+
+            }
+
+
+        }
 
 
 
@@ -44,5 +89,5 @@ public class SergioReviewLanaTest {
         }
 
 
-
+    //git commit -m "Add SergioReviewLanaTest.java"
 }
