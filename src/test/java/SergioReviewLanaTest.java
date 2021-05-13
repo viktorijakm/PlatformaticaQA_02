ab6feaacc20abc272827d829dafc31379cced226
Penny-Hofstadter <rnasibullina@gmail.com>
2021-05-13 10:25:25 -0400

    Add SergioReviewLanaTest.java

---
+++ src/test/java/SergioReviewLanaTest.java
@@ -0,0 +1,47 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SergioReviewLanaTest {
+
+
+
+
+        private WebDriver driver;
+
+        @BeforeClass
+        public void before() {
+            WebDriverManager.chromedriver().setup();
+        }
+
+        @BeforeMethod
+        public void beforeTest() {
+            driver = new ChromeDriver();
+        }
+
+        @Test
+        public void  review() {
+            driver.get("http://shop.demoqa.com/my-account/");
+            driver.findElement(By.linkText("Lost your password?")).click();
+         driver.navigate().to("http://shop.demoqa.com/my-account");
+         driver.navigate().back();
+         driver.navigate().forward();
+         driver.navigate().refresh();
+        }
+
+
+
+        @AfterMethod
+        public void afterTest() {
+            driver.quit();
+        }
+
+
+}
