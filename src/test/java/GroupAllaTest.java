1dbea2371d84f814a822ec621f0b4a89d90ab8fe
G-Aleksey <80530491+G-Aleksey@users.noreply.github.com>
2021-05-13 22:23:34 +0300

    Group alla (#115)
    * Fix error2
    
    * Merged

--- src/test/java/GroupAllaTest.java
+++ src/test/java/GroupAllaTest.java
@@ -8,21 +8,22 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
 
 public class GroupAllaTest {
-
-    private WebDriver driver;
 
     @BeforeClass
     public void before() {
         WebDriverManager.chromedriver().setup();
     }
 
+    private WebDriver driver;
+
     @BeforeMethod
     public void beforeTest() {
         driver = new ChromeDriver();
-
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
     @Test
@@ -41,20 +42,22 @@ public class GroupAllaTest {
 
     }
 
-      @Test
+    @Test
     public void testGunchenkoAlex() {
+
         driver.get("https://www.google.com/");
         WebElement input = driver.findElement(By.className("gLFyf"));
         input.sendKeys("gismeteo\n");
-        WebElement result1 = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div[1]/a/h3"));
-        Assert.assertEquals(result1.getText(), "GISMETEO: Погода в Украине, прогноз погоды на сегодня ...");
-        WebElement GismeteoButton = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div[1]/a/h3"));
+
+        WebElement GismeteoButton = driver.findElement(By.xpath("//h3[contains(text(),'GISMETEO:')]"));
         GismeteoButton.click();
+
         WebElement searchField = driver.findElement(By.id("js-search"));
         searchField.sendKeys("Харьков\n");
         driver.findElement(By.linkText("Харьков")).click();
         driver.findElement(By.xpath("/html/body/section/nav/div/ul/li[6]/a")).click();
         driver.findElement(By.xpath("/html/body/section/nav/div/ul/li[7]/a")).click();
+
         WebElement pageTitile = driver.findElement(By.tagName("h1"));
         String pageTitleName = pageTitile.getText();
         Assert.assertEquals(pageTitleName, "Погода в аэропорту Харьков на месяц");
@@ -94,5 +97,3 @@ public class GroupAllaTest {
         driver.quit();
     }
 }
-
-
