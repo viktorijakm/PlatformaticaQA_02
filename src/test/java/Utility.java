b79f52a1d9d6c150e81ed79760e607f029795375
ValeriiSarukhanov <valerapromo@gmail.com>
2021-05-15 22:33:40 -0400

    Refactor

--- src/test/java/Utility.java
+++ src/test/java/Utility.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -7,17 +9,17 @@ import org.testng.annotations.BeforeMethod;
 
 public abstract class Utility {
 
-    private WebDriver driver;
+    public WebDriver driver;
 
-    final WebDriver getDriver() {
-        return driver;
+    public static void newClick (WebDriver driver, WebElement element) {
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
     }
 
     @BeforeClass
     public void beforeClass() {
         WebDriverManager.chromedriver().setup();
     }
-
 
     @BeforeMethod
     public void beforeMethod() {
