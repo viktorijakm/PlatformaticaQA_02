3479d1ba4e7bf44dcdbd878f0a01b4634a5484e1
Daria Altukhova <altukhova.daria@gmail.com>
2021-05-16 22:54:10 -0700

    Extend Utility class

--- src/test/java/DariaAltukhovaTest.java
+++ src/test/java/DariaAltukhovaTest.java
@@ -1,29 +1,12 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class DariaAltukhovaTest {
+public class DariaAltukhovaTest extends Utility {
 
-    @BeforeClass
-    public void before() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    private WebDriver driver;
     final String mainURL = "https://www.edcast.com/";
     final String demoUrl = "https://www.edcast.com/corp/request-a-demo/";
-
-    @BeforeMethod
-    public void beforeTest() {
-        driver = new ChromeDriver();
-    }
 
     @Test
     public void testDariaAltukhovaOpenDemoFromPage()  {
@@ -57,10 +40,5 @@ public class DariaAltukhovaTest {
         watchEdcastDemoVideoButton.click();
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://go.edcast.com/l/139221/2021-03-31/2m9x9x");
-    }
-
-    @AfterMethod
-    public void afterTest() {
-        driver.quit();
     }
 }
